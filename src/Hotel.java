ef43f6bbff85c7b2bf859b1ddfdf52772e5b0c01
TomoriComAncora <lucas_fernando@ufms.br>
2025-09-14 23:27:29 -0400

    função de reservar quarto e listar

--- src/Hotel.java
+++ src/Hotel.java
@@ -3,9 +3,11 @@ import com.reserva.entity.Reserva;
 import java.util.Scanner;
 
 public class Hotel {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
+    Reserva[] reservas = new Reserva[10];
     int opcao = 0;
+    int contatdorDeReservas = 0;
     do {
       System.out.println("Escolha uma opção: ");
       System.out.println(
@@ -17,19 +19,31 @@ public class Hotel {
       } else {
         switch (opcao) {
           case 1:
-            System.out.println("Digite seu nome:");
-            String nome = scanner.nextLine();
-            System.out.println("Quantos dias de estadia: ");
-            int numeroDeDias = scanner.nextInt();
-            System.out.println("Valor da sua diária: ");
-            double valorDiaria = scanner.nextDouble();
-            System.out.println("Qual tipo de quarto? Standard-100R$, Luxo-200R$ ou Presidencial-350R$");
-            String tipoQuarto = scanner.nextLine();
-            Reserva reserva = new Reserva(nome, numeroDeDias, valorDiaria);
-            System.out.println(reserva);
+            if(contatdorDeReservas > 9){
+                throw new Exception("Não há mais vagas a serem reservadas");
+            }else {
+                System.out.println("Digite seu nome:");
+                String nome = scanner.nextLine();
+                System.out.println("Quantos dias de estadia: ");
+                int numeroDeDias = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Qual tipo de quarto? Standard - 110,80 R$, Luxo - 290, 30 R$ ou Presidencial - 459,99 R$");
+                String tipoQuarto = scanner.nextLine();
+                Reserva reserva = new Reserva(nome, numeroDeDias, tipoQuarto);
+                reservas[contatdorDeReservas] = reserva;
+                contatdorDeReservas++;
+                Reserva.setQuantidadeReservas(1);
+                reserva.setValorDiaria(numeroDeDias, tipoQuarto);
+                System.out.println("Restam: " + (reservas.length - reserva.getQuantidadeReservas()) + " vagas");
+            }
             break;
           case 2:
             System.out.println("Lista de reservas");
+            for (int i = 0; i < contatdorDeReservas; i++) {
+              System.out.println(reservas[i]);
+              System.out.println("Valor total da diária: " + reservas[i].getValorDiaria());
+              System.out.println("------------------------------------");
+            }
             break;
           case 3:
             System.out.println("Buscar reserva por nome");
--- src/com/reserva/entity/Reserva.java
+++ src/com/reserva/entity/Reserva.java
@@ -7,20 +7,73 @@ public class Reserva {
   private double valorDiaria;
   private static int quantidadeReservas = 0;
 
-  public Reserva(String nome, int numeroDeDias, double valorDiaria) {
-    this.nome = nome;
-    this.numeroDeDias = numeroDeDias;
-    this.valorDiaria = valorDiaria;
+  public Reserva(String nome, int numeroDeDias, String tipoDeQuarto) throws Exception {
+    this.nome = validarNome(nome);
+    this.numeroDeDias = validarDias(numeroDeDias);
+    this.tipoDeQuarto = validarQuartos(tipoDeQuarto);
   }
 
-  @Override
+  //  public Reserva(String nome, String tipoDeQuarto){
+  //      this.nome = nome;
+  //      this.tipoDeQuarto = tipoDeQuarto;
+  //  }
+
+  public int getQuantidadeReservas() {
+    return quantidadeReservas;
+  }
+
+  public static void setQuantidadeReservas(int quantidadeReservas) {
+    Reserva.quantidadeReservas += quantidadeReservas;
+  }
+
+  private String validarNome(String nome) throws Exception {
+    if (nome.isEmpty()) {
+      throw new Exception("Nome inválido!");
+    }
+    return nome;
+  }
+
+  private int validarDias(int numeroDeDias) throws Exception {
+    if (numeroDeDias <= 0) {
+      throw new Exception("Dias de estadias inválidos");
+    }
+    return numeroDeDias;
+  }
+
+  private String validarQuartos(String tipoDeQuarto) throws Exception {
+    if (tipoDeQuarto.equalsIgnoreCase("standard")
+        || tipoDeQuarto.equalsIgnoreCase("luxo")
+        || tipoDeQuarto.equalsIgnoreCase("presidencial")) {
+      return tipoDeQuarto;
+    } else {
+      throw new Exception("Quarto não encotrado!");
+    }
+  }
+
+  private double calcularValorTotal(int numeroDeDias, String tipoDeQuarto) throws Exception {
+      return switch (tipoDeQuarto) {
+          case "standard" -> this.valorDiaria = numeroDeDias * 110.80;
+          case "luxo" -> this.valorDiaria = numeroDeDias * 290.30;
+          case "presidencial" -> this.valorDiaria = numeroDeDias * 459.99;
+          default -> throw new Exception("Quarto inválido");
+      };
+  }
+
+    public double getValorDiaria() {
+        return valorDiaria;
+    }
+
+    public void setValorDiaria(int numeroDeDias, String tipoDeQuarto) throws Exception {
+      this.valorDiaria = calcularValorTotal(numeroDeDias, tipoDeQuarto);
+    }
+
+
+    @Override
   public String toString() {
     return "Nome do cliente: "
         + nome
-        + "\nQuarto do cliente: "
+        + "\nQuarto escolhido: "
         + tipoDeQuarto
-        + "\nValor da diária: "
-        + valorDiaria
         + "\nNúmero de dias de estadia: "
         + numeroDeDias;
   }
