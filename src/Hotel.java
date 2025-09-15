6d7a6fcea61c0571be5b51a3c652af3d39cd9292
TomoriComAncora <lucas_fernando@ufms.br>
2025-09-14 20:39:52 -0400

    criando menu

--- src/Hotel.java
+++ src/Hotel.java
@@ -1,5 +1,51 @@
-public class Hotel {
-    public static void main(String[] args){
+import com.reserva.entity.Reserva;
 
-    }
+import java.util.Scanner;
+
+public class Hotel {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int opcao = 0;
+    do {
+      System.out.println("Escolha uma opção: ");
+      System.out.println(
+          "1 - Nova reserva \n2 - Listar reserva \n3 - Buscar reserva por nome do hóspede \n4 - Ordernar reserva por número de dias \n5 - Sair");
+      opcao = scanner.nextInt();
+      scanner.nextLine();
+      if (opcao < 0 || opcao > 5) {
+        System.out.println("Por favor, escolha um número entre 1 e 5");
+      } else {
+        switch (opcao) {
+          case 1:
+            System.out.println("Digite seu nome:");
+            String nome = scanner.nextLine();
+            System.out.println("Quantos dias de estadia: ");
+            int numeroDeDias = scanner.nextInt();
+            System.out.println("Valor da sua diária: ");
+            double valorDiaria = scanner.nextDouble();
+            System.out.println("Qual tipo de quarto? Standard-100R$, Luxo-200R$ ou Presidencial-350R$");
+            String tipoQuarto = scanner.nextLine();
+            Reserva reserva = new Reserva(nome, numeroDeDias, valorDiaria);
+            System.out.println(reserva);
+            break;
+          case 2:
+            System.out.println("Lista de reservas");
+            break;
+          case 3:
+            System.out.println("Buscar reserva por nome");
+            break;
+          case 4:
+            System.out.println("Ordenar as reservas");
+            break;
+          case 5:
+            System.out.println("Sair");
+            break;
+          default:
+            System.out.println("opção inválida");
+            break;
+        }
+      }
+
+    } while (opcao != 5);
+  }
 }
--- src/com/reserva/entity/Reserva.java
+++ src/com/reserva/entity/Reserva.java
@@ -1,3 +1,27 @@
 package com.reserva.entity;
 
-public class Reserva {}
+public class Reserva {
+  private String nome;
+  private String tipoDeQuarto;
+  private int numeroDeDias;
+  private double valorDiaria;
+  private static int quantidadeReservas = 0;
+
+  public Reserva(String nome, int numeroDeDias, double valorDiaria) {
+    this.nome = nome;
+    this.numeroDeDias = numeroDeDias;
+    this.valorDiaria = valorDiaria;
+  }
+
+  @Override
+  public String toString() {
+    return "Nome do cliente: "
+        + nome
+        + "\nQuarto do cliente: "
+        + tipoDeQuarto
+        + "\nValor da diária: "
+        + valorDiaria
+        + "\nNúmero de dias de estadia: "
+        + numeroDeDias;
+  }
+}
